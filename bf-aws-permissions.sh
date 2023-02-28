6a32eed538a160eb28235bd1aa43f62e77eb2ee5
carlospolop <carlospolop@gail.com>
2023-02-28 02:04:49 +0100

    all svcs

--- bf-aws-permissions.sh
+++ bf-aws-permissions.sh
@@ -113,10 +113,9 @@ test_command() {
 
 
 # BFS through the AWS services and get-list-describe 
-#for service in $(get_aws_services); do
-    service="ssm"
+for service in $(get_aws_services); do
     for svc_command in $(get_commands_for_service "$service"); do
         test_command "$service" "$svc_command" &
     done
     wait
-#done
+done
